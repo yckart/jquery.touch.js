4a34cbee6cf8cb6b59d8772a168e6378eeb737c6
yckart <mail@yckart.com>
2013-02-26 05:58:38 +0100

    fixed some failures

--- jquery.touch.js
+++ jquery.touch.js
@@ -1,154 +1,183 @@
 /*!
- * jquery.touch.js 0.0.1 - https://github.com/yckart/jquery.touch.js
- * A lightweight mobile-ready touchslider.
+ * jquery.touch.js 0.0.5 - https://github.com/yckart/jquery.touch.js
+ * Drag, scale and rotate elements during touch.
  *
  * Copyright (c) 2013 Yannick Albert (http://yckart.com)
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php).
- * 2013/02/09
-*/
-(function ($, window) {
-    if(window.jQuery) $.event.props.push("touches");
-    function bezier_for_velocity(v) {
-        var importance = 0.5,
-            x = (v < 0 ? -1 : 1) * Math.sqrt(importance * importance * (v * v / (1 + v * v))),
-            t = x / v,
-            sameness = t;
+ * 2013/02/23
+ */
 
-        return 'cubic-bezier(' + [t, x, sameness, 1.0].join(", ") + ')';
+(function($, TweenLite) {
+    "use strict";
+
+    var defaults = {
+        draggable: true,
+        rotatable: true,
+        scalable: true,
+        tween: {
+            use: false,
+            speed: 1,
+            ease: ''
+        },
+        touchClass: 'touching'
+    };
+
+    var props = $.event.props || [];
+    props.push("touches");
+    props.push("scale");
+    props.push("rotation");
+    $.event.props = props;
+
+    $.fn.getMatrix = function(i) {
+        if(this.css('transform') === 'none') return 0;
+        var array = this.css('transform').split('(')[1].split(')')[0].split(',');
+        return array[i] || array;
+    };
+
+    function Touch(elem, options) {
+
+        this.elem = elem;
+        this.$elem = $(elem);
+
+        this.options = $.extend({}, defaults, options);
+
+        //detect support for Webkit CSS 3d transforms
+        this.supportsWebkit3dTransform = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix());
+
+        this.init();
     }
 
-    $.fn.touchSlider = function () {
-        var x, t,
-            initial_x, initial_offset,
-            previous_x, previous_t,
-            current_offset = 0,
-            $this = this,
-            slides = this.find(".slides"),
-            animation = {},
-            default_duration = 500,
-            slide_width = this.width(),
-            last_slide = this.find(".slide").length - 1,
-            left_edge = 0,
-            right_edge = slide_width * last_slide;
+    //static property to store the zIndex for an element
+    Touch.zIndexCount = 1;
 
-        this.css({
-            overflow: 'hidden'
-        });
-        slides.css({
-            'width': (slide_width * this.find(".slide").length) + 'px',
-            '-webkit-transition-property': '-webkit-transform',
-            '-webkit-transform': 'translate3D(0, 0, 0)'
-        });
-        // Force slides into texture buffers on the iPad
-        slides.find(".slide").css({
-            '-webkit-transform': 'translate3D(0, 0, 0)'
-        });
-
-        this.bind('touchstart', function (e) {
-
-            if (e.touches.length !== 1) {
-                return;
-            }
-
-            // Ensure that if the user stops the animation half way through, our internal
-            // state is correct.
-            // getComputedStyle seems to always return the result in the form: matrix(0, 0, 0, 0, X, 0)
-            current_offset = 0 - Number(window.getComputedStyle(slides[0])['-webkit-transform'].split(", ")[4]);
-
-            slides.css({
-                // Remove the delay on animation for instant finger feedback
-                '-webkit-transition-duration': '0s',
-                '-webkit-transform': 'translate3D(' + (0 - current_offset) + 'px, 0, 0)'
-            });
-
-            initial_offset = current_offset;
-            initial_x = previous_x = x = e.touches[0].clientX;
-            previous_t = t = +new Date();
-
-        }).on('touchmove', function (e) {
-
-            // Allow the user to zoom when they're touching the container by not
-            // preventing default if there's more than one finger.
-            if (e.touches.length !== 1) {
-                return;
-            } else {
-                e.preventDefault();
-            }
-
-            previous_x = x;
-            previous_t = t;
-            x = e.touches[0].clientX;
-            t = +new Date();
-
-            current_offset = initial_offset + (initial_x - x);
-
-            // iOS-like halved velocity when dragging beyond the edge
-            if (current_offset < left_edge) {
-                current_offset = current_offset / 2;
-            } else if (current_offset > right_edge) {
-                current_offset = right_edge + (current_offset - right_edge) / 2;
-            }
-
-            slides.css({
-                '-webkit-transform': 'translate3D(' + (0 - current_offset) + 'px, 0, 0)'
-            });
-
-        }).on('touchend', function (e) {
-            var velocity;
-
-            var final_destination = current_offset;
-
-            // Move up to half an extra slide in the direction of current motion.
-            // TODO: this will feel much nicer once we have bouncing.
-            if (Math.abs(t - previous_t) > 1) {
-                final_destination += Math.min(slide_width / 2,
-                                       Math.max(-slide_width / 2,
-                                         default_duration * (previous_x - x) / (t - previous_t)
-                                       )
-                                     );
-            }
-
-            var target_slide = Math.round(final_destination / slide_width);
-
-            if (target_slide < 0) {
-                target_slide = 0;
-            } else if (target_slide > last_slide) {
-                target_slide = last_slide;
-            }
-
-            var target_distance = current_offset - target_slide * slide_width;
-
-            // avoid division by zero
-            if (Math.abs(x - previous_x) < 1 || Math.abs(t - previous_t) < 1) {
-                velocity = 0.1;
-            } else {
-                velocity = ((x - previous_x) / (t - previous_t)) * (default_duration / target_distance);
-            }
-
-            $this.trigger('slideTo', {slide: target_slide, bezier: bezier_for_velocity(velocity)});
-
-        }).on('slideTo', function (e, opts) {
-            var target_offset = opts.slide * slide_width;
-
-            animation = {
-                slide: opts.slide,
-                // webkit doesn't perform the transition if the duration is 0 and the slider is offscreen
-                duration: Math.max(typeof opts.duration === 'undefined' ? default_duration : opts.duration, 1),
-                bezier: opts.bezier || bezier_for_velocity(0.1)
-            };
-
-            slides.css({
-                '-webkit-transition-timing-function': animation.bezier,
-                '-webkit-transition-duration': animation.duration + 'ms',
-                '-webkit-transform': 'translate3D(' + (0 - target_offset) + 'px, 0, 0)'
-            });
-
-            $this.trigger('beforeSlide', animation);
-            animation.timeout = window.setTimeout(function () {
-                $this.trigger('afterSlide', animation);
-            }, animation.duration);
-
-        });
-        return this;
+    Touch.prototype.init = function() {
+        this.rotation = 0; //default rotation in degrees
+        this.scale = 1.0; //default scale value
+        this.gesture = false; //flags a 2 touch gesture
+        this.$elem.on('touchstart', this.touchstart.bind(this));
     };
-}(window.jQuery || window.Zepto, window));
+
+    Touch.prototype.touchstart = function(e) {
+        e.preventDefault();
+
+        //bring item to the front
+        this.elem.style.zIndex = Touch.zIndexCount++;
+        this.$elem.on('touchmove.touch', this.touchmove.bind(this));
+        this.$elem.on('touchend.touch touchcancel.touch', this.touchend.bind(this));
+
+        if(this.options.touchClass) {
+            this.$elem.addClass(this.options.touchClass);
+        }
+
+        this.start0X = this.$elem.getMatrix(4) - e.touches[0].pageX;
+        this.start0Y = this.$elem.getMatrix(5) - e.touches[0].pageY;
+        if(e.touches.length < 2) return;
+        this.start1X = this.$elem.getMatrix(4) - e.touches[1].pageX;
+        this.start1Y = this.$elem.getMatrix(5) - e.touches[1].pageY;
+
+    };
+
+    Touch.prototype.touchmove = function(e) {
+
+        e.preventDefault();
+
+        var myTransform = "",
+            x1 = 0,
+            y1 = 0,
+            x2 = 0,
+            y2 = 0,
+            curX = 0,
+            curY = 0;
+
+        //drag event
+        if(e.touches.length === 1) {
+
+            //get drag point
+            curX = this.start0X + e.touches[0].pageX;
+            curY = this.start0Y + e.touches[0].pageY;
+
+            //translate drag
+            if(this.options.draggable) {
+                if(this.supportsWebkit3dTransform) {
+                    myTransform += 'translate3d(' + curX + 'px,' + curY + 'px, 0)';
+                } else {
+                    myTransform += 'translate(' + curX + 'px,' + curY + 'px)';
+                }
+            }
+
+            //persist scale and rotate values from previous gesture
+            if(this.options.scalable) {
+                myTransform += "scale(" + (this.scale) + ")";
+            }
+
+            if(this.options.rotatable) {
+                myTransform += "rotate(" + ((this.rotation) % 360) + "deg)";
+            }
+        } else if(e.touches.length === 2) {
+
+            //gesture event
+            this.gesture = true;
+
+            //get middle point between two touches for drag
+            x1 = this.start0X + e.touches[0].pageX;
+            y1 = this.start0Y + e.touches[0].pageY;
+            x2 = this.start1X + e.touches[1].pageX;
+            y2 = this.start1Y + e.touches[1].pageY;
+            curX = (x1 + x2) / 2, curY = (y1 + y2) / 2;
+
+            //translate drag
+            if(this.options.draggable) {
+                if(this.supportsWebkit3dTransform) {
+                    myTransform += 'translate3d(' + curX + 'px,' + curY + 'px, 0)';
+                } else {
+                    myTransform += 'translate(' + curX + 'px,' + curY + 'px)';
+                }
+            }
+
+            //scale and rotate
+            if(this.options.scalable) {
+                myTransform += "scale(" + (this.scale * e.scale) + ")";
+            }
+
+            if(this.options.rotatable) {
+                myTransform += "rotate(" + ((this.rotation + e.rotation) % 360) + "deg)";
+            }
+        }
+
+        if(this.options.tween.use) {
+            TweenLite.to(this.elem, this.options.tween.speed, {
+                css: { transform: myTransform },
+                ease: this.options.tween.ease
+            });
+        } else {
+            this.elem.style.webkitTransform = this.elem.style.MozTransform = this.elem.style.msTransform = this.elem.style.OTransform = this.elem.style.transform = myTransform;
+        }
+    };
+
+    Touch.prototype.touchend = function(e) {
+
+        e.preventDefault();
+
+        this.$elem.off('.touch');
+
+        //store scale and rotate values on gesture end
+        if(this.gesture) {
+            this.scale *= e.scale;
+            this.rotation = (this.rotation + e.rotation) % 360;
+            this.gesture = false;
+        }
+
+        if(this.options.touchClass) {
+            this.$elem.removeClass(this.options.touchClass);
+        }
+    };
+
+    // plugin wrapper
+    $.fn.touch = function(options) {
+        return this.each(function() {
+            if (!$.data(this, "plugin_touch")) {
+                $.data(this, 'plugin_touch', new Touch(this, options));
+            }
+        });
+    };
+}(jQuery, window.TweenLite));
